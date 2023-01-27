446f792792eb6cad099834846abd9bc5c8648ba3
ani <amazinganirudhhere@gmail.com>
2023-01-27 12:13:36 +0530

    date fetched in backend

--- server.py
+++ server.py
@@ -1,22 +1,23 @@
-#Make sure that flask_login and bcrypt are installed
-from flask_login import login_user,logout_user,current_user,UserMixin
+# Make sure that flask_login and bcrypt are installed
+from flask_login import login_user, logout_user, current_user, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bcrypt import Bcrypt
-from flask import Flask,redirect,url_for,render_template,request, Response
+from flask import Flask, redirect, url_for, render_template, request, Response
 import io
 import xlwt
-import psycopg2 
+import psycopg2
 import psycopg2.extras
 
-#Position all of this after the db and app have been initialised
-app=Flask(__name__)
+# Position all of this after the db and app have been initialised
+app = Flask(__name__)
 bcrypt = Bcrypt(app)
 user = 'postgres'
 password = 'Jivi1122'
 host = '127.0.0.1'
 port = 5432
 database = 'testdb'
-conn = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
+conn = psycopg2.connect(dbname=database, user=user,
+                        password=password, host=host, port=port)
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 # @login_manager.user_loader
@@ -25,168 +26,169 @@ conn = psycopg2.connect(dbname=database, user=user, password=password, host=host
 #     return User.query.get(user_id)
 
 # A route to display the home page
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def home():
-    if request.method=='POST':
-        # Handle POST Request here
-        return render_template('index.html')
+    #     if request.method=='POST':
+    #         # Handle POST Request here
+    #         return render_template('index.html')
     return render_template('index.html')
 
-@app.route('/db', methods=['POST', 'GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def getData():
-    if request.method=='POST':
-        From= request.form['from']
-        To= request.form['to']
-    # params = request.get_json()
-    # print()
-    # print(params)
-    # print()
-    # From, To = params['From'].strip(), params['To'].strip()
-    output = io.BytesIO()
-    workbook = xlwt.Workbook()
-    toiletSh = workbook.add_sheet('toilet feedbacks')
-    foodSh = workbook.add_sheet('foood feedbacks')
-    securitySh = workbook.add_sheet('security feedbacks')
-    buildingSh = workbook.add_sheet('building feedbacks')
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("SELECT * from basicInfo INNER JOIN toiletForm on toiletForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
-        ", str(From), " AND basicInfo.SubmitTime <=", str(To))
-    toiletF = cur.fetchall()
-    cur.execute("SELECT * from basicInfo INNER JOIN foodForm on foodForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
-        ", str(From), " AND basicInfo.SubmitTime <= ", str(To))
-    foodF = cur.fetchall()
-    cur.execute("SELECT * from basicInfo INNER JOIN securityForm on securityForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
-        ", str(From), " AND basicInfo.SubmitTime <= ", str(To))
+    if request.method == 'POST':
+        From = request.form['From']
+        To = request.form['To']
+        print("\n")
+        print(From, To)
+        print("\n")
 
-    securityF = cur.fetchall()
-    cur.execute("SELECT * from basicInfo INNER JOIN buildingForm on buildingForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
-        ", str(From), " AND basicInfo.SubmitTime <= ", str(To))
+        output = io.BytesIO()
+        workbook = xlwt.Workbook()
+        toiletSh = workbook.add_sheet('toilet feedbacks')
+        foodSh = workbook.add_sheet('foood feedbacks')
+        securitySh = workbook.add_sheet('security feedbacks')
+        buildingSh = workbook.add_sheet('building feedbacks')
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("SELECT * from basicInfo INNER JOIN toiletForm on toiletForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
+            ", str(From), " AND basicInfo.SubmitTime <=", str(To))
+        toiletF = cur.fetchall()
+        cur.execute("SELECT * from basicInfo INNER JOIN foodForm on foodForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
+            ", str(From), " AND basicInfo.SubmitTime <= ", str(To))
+        foodF = cur.fetchall()
+        cur.execute("SELECT * from basicInfo INNER JOIN securityForm on securityForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
+            ", str(From), " AND basicInfo.SubmitTime <= ", str(To))
 
-    buildingF = cur.fetchall()
+        securityF = cur.fetchall()
+        cur.execute("SELECT * from basicInfo INNER JOIN buildingForm on buildingForm.id = basicInfo.id WHERE basicInfo.SubmitTime >= \
+            ", str(From), " AND basicInfo.SubmitTime <= ", str(To))
 
-    toiletSh.write(0, 0, 'Id')
-    toiletSh.write(0, 1, 'Building')
-    toiletSh.write(0, 2, 'Floor')
-    toiletSh.write(0, 3, 'Name')
-    toiletSh.write(0, 4, 'Phone')
-    toiletSh.write(0, 5, 'Type')
-    toiletSh.write(0, 6, 'Cleanliness')
-    toiletSh.write(0, 7, 'Complaint')
-    toiletSh.write(0, 8, 'Flushworking')
-    toiletSh.write(0, 9, 'WashedRegularly')
-    toiletSh.write(0, 10, 'WaterLeakage')
-    toiletSh.write(0, 11, 'WaterSupply')
-    # print(toiletF)
-    idx = 0
-    for row in toiletF:
-        toiletSh.write(idx+1, 0, str(row[0]))
-        toiletSh.write(idx+1, 1, str(row[1]))
-        toiletSh.write(idx+1, 2, row[2])
-        toiletSh.write(idx+1, 3, row[3])
-        toiletSh.write(idx+1, 4, row[4])
-        toiletSh.write(idx+1, 5, row[5])
-        toiletSh.write(idx+1, 6, row[7])
-        toiletSh.write(idx+1, 7, row[8])
-        toiletSh.write(idx+1, 8, row[9])
-        toiletSh.write(idx+1, 9, row[10])
-        toiletSh.write(idx+1, 10, row[11])
-        toiletSh.write(idx+1, 11, row[12])
-        # toiletSh.write(idx+1, 4, row['Phone'])
-        idx += 1
+        buildingF = cur.fetchall()
 
-    foodSh.write(0, 0, 'Id')
-    foodSh.write(0, 1, 'Building')
-    foodSh.write(0, 2, 'Floor')
-    foodSh.write(0, 3, 'Name')
-    foodSh.write(0, 4, 'Phone')
-    foodSh.write(0, 5, 'Type')
-    foodSh.write(0, 6, 'Ambience')
-    foodSh.write(0, 7, 'Cleanliness')
-    foodSh.write(0, 8, 'Feedback')
-    foodSh.write(0, 9, 'FoodQuality')
-    foodSh.write(0, 10, 'FoodTaste')
-    foodSh.write(0, 11, 'ServiceQuality')
+        toiletSh.write(0, 0, 'Id')
+        toiletSh.write(0, 1, 'Building')
+        toiletSh.write(0, 2, 'Floor')
+        toiletSh.write(0, 3, 'Name')
+        toiletSh.write(0, 4, 'Phone')
+        toiletSh.write(0, 5, 'Type')
+        toiletSh.write(0, 6, 'Cleanliness')
+        toiletSh.write(0, 7, 'Complaint')
+        toiletSh.write(0, 8, 'Flushworking')
+        toiletSh.write(0, 9, 'WashedRegularly')
+        toiletSh.write(0, 10, 'WaterLeakage')
+        toiletSh.write(0, 11, 'WaterSupply')
+        # print(toiletF)
+        idx = 0
+        for row in toiletF:
+            toiletSh.write(idx+1, 0, str(row[0]))
+            toiletSh.write(idx+1, 1, str(row[1]))
+            toiletSh.write(idx+1, 2, row[2])
+            toiletSh.write(idx+1, 3, row[3])
+            toiletSh.write(idx+1, 4, row[4])
+            toiletSh.write(idx+1, 5, row[5])
+            toiletSh.write(idx+1, 6, row[7])
+            toiletSh.write(idx+1, 7, row[8])
+            toiletSh.write(idx+1, 8, row[9])
+            toiletSh.write(idx+1, 9, row[10])
+            toiletSh.write(idx+1, 10, row[11])
+            toiletSh.write(idx+1, 11, row[12])
+            # toiletSh.write(idx+1, 4, row['Phone'])
+            idx += 1
 
-    idx = 0
-    for row in foodF:
-        foodSh.write(idx+1, 0, str(row[0]))
-        foodSh.write(idx+1, 1, str(row[1]))
-        foodSh.write(idx+1, 2, row[2])
-        foodSh.write(idx+1, 3, row[3])
-        foodSh.write(idx+1, 4, row[4])
-        foodSh.write(idx+1, 5, row[5])
-        foodSh.write(idx+1, 6, row[7])
-        foodSh.write(idx+1, 7, row[8])
-        foodSh.write(idx+1, 8, row[9])
-        foodSh.write(idx+1, 9, row[10])
-        foodSh.write(idx+1, 10, row[11])
-        foodSh.write(idx+1, 11, row[12])
-        idx+=1
+        foodSh.write(0, 0, 'Id')
+        foodSh.write(0, 1, 'Building')
+        foodSh.write(0, 2, 'Floor')
+        foodSh.write(0, 3, 'Name')
+        foodSh.write(0, 4, 'Phone')
+        foodSh.write(0, 5, 'Type')
+        foodSh.write(0, 6, 'Ambience')
+        foodSh.write(0, 7, 'Cleanliness')
+        foodSh.write(0, 8, 'Feedback')
+        foodSh.write(0, 9, 'FoodQuality')
+        foodSh.write(0, 10, 'FoodTaste')
+        foodSh.write(0, 11, 'ServiceQuality')
 
-    securitySh.write(0, 0, 'Id')
-    securitySh.write(0, 1, 'Building')
-    securitySh.write(0, 2, 'Floor')
-    securitySh.write(0, 3, 'Name')
-    securitySh.write(0, 4, 'Phone')
-    securitySh.write(0, 5, 'Type')
-    securitySh.write(0, 6, 'SecurityAlertness')
-    securitySh.write(0, 7, 'SecurityAvailability')
-    securitySh.write(0, 8, 'SecurityDrunk')
-    securitySh.write(0, 9, 'SecurityMisbehaving')
-    
-    idx = 0
-    for row in securityF:
-        securitySh.write(idx+1, 0, str(row[0]))
-        securitySh.write(idx+1, 1, str(row[1]))
-        securitySh.write(idx+1, 2, row[2])
-        securitySh.write(idx+1, 3, row[3])
-        securitySh.write(idx+1, 4, row[4])
-        securitySh.write(idx+1, 5, row[5])
-        securitySh.write(idx+1, 6, row[7])
-        securitySh.write(idx+1, 7, row[8])
-        securitySh.write(idx+1, 8, row[9])
-        securitySh.write(idx+1, 9, row[10])
-        idx+=1
+        idx = 0
+        for row in foodF:
+            foodSh.write(idx+1, 0, str(row[0]))
+            foodSh.write(idx+1, 1, str(row[1]))
+            foodSh.write(idx+1, 2, row[2])
+            foodSh.write(idx+1, 3, row[3])
+            foodSh.write(idx+1, 4, row[4])
+            foodSh.write(idx+1, 5, row[5])
+            foodSh.write(idx+1, 6, row[7])
+            foodSh.write(idx+1, 7, row[8])
+            foodSh.write(idx+1, 8, row[9])
+            foodSh.write(idx+1, 9, row[10])
+            foodSh.write(idx+1, 10, row[11])
+            foodSh.write(idx+1, 11, row[12])
+            idx += 1
 
-    buildingSh.write(0, 0, 'Id')
-    buildingSh.write(0, 1, 'Building')
-    buildingSh.write(0, 2, 'Floor')
-    buildingSh.write(0, 3, 'Name')
-    buildingSh.write(0, 4, 'Phone')
-    buildingSh.write(0, 5, 'Type')
-    buildingSh.write(0, 6, 'Cleanliness')
-    buildingSh.write(0, 7, 'Feedback')
+        securitySh.write(0, 0, 'Id')
+        securitySh.write(0, 1, 'Building')
+        securitySh.write(0, 2, 'Floor')
+        securitySh.write(0, 3, 'Name')
+        securitySh.write(0, 4, 'Phone')
+        securitySh.write(0, 5, 'Type')
+        securitySh.write(0, 6, 'SecurityAlertness')
+        securitySh.write(0, 7, 'SecurityAvailability')
+        securitySh.write(0, 8, 'SecurityDrunk')
+        securitySh.write(0, 9, 'SecurityMisbehaving')
 
-    idx = 0
-    
-    for row in buildingF:
-        buildingSh.write(idx+1, 0, str(row[0]))
-        buildingSh.write(idx+1, 1, str(row[1]))
-        buildingSh.write(idx+1, 2, row[2])
-        buildingSh.write(idx+1, 3, row[3])
-        buildingSh.write(idx+1, 4, row[4])
-        buildingSh.write(idx+1, 5, row[5])
-        buildingSh.write(idx+1, 6, row[7])
-        buildingSh.write(idx+1, 7, row[8])
-        idx+=1
+        idx = 0
+        for row in securityF:
+            securitySh.write(idx+1, 0, str(row[0]))
+            securitySh.write(idx+1, 1, str(row[1]))
+            securitySh.write(idx+1, 2, row[2])
+            securitySh.write(idx+1, 3, row[3])
+            securitySh.write(idx+1, 4, row[4])
+            securitySh.write(idx+1, 5, row[5])
+            securitySh.write(idx+1, 6, row[7])
+            securitySh.write(idx+1, 7, row[8])
+            securitySh.write(idx+1, 8, row[9])
+            securitySh.write(idx+1, 9, row[10])
+            idx += 1
 
-    workbook.save(output)
-    output.seek(0)
- 
-    return Response(output, mimetype="application/ms-excel", headers={"Content-Disposition":"attachment;filename=feedbacks.xls"})
-    
-#TODO preethivi: integrate the QR code function to the server and Generate QR code when they hit sumbit
+        buildingSh.write(0, 0, 'Id')
+        buildingSh.write(0, 1, 'Building')
+        buildingSh.write(0, 2, 'Floor')
+        buildingSh.write(0, 3, 'Name')
+        buildingSh.write(0, 4, 'Phone')
+        buildingSh.write(0, 5, 'Type')
+        buildingSh.write(0, 6, 'Cleanliness')
+        buildingSh.write(0, 7, 'Feedback')
 
-#TODO preethivi: add a route to display the feedback data
+        idx = 0
 
-#TODO preethivi: in the feedback display route, add a filter to select feedbacks between selected date
+        for row in buildingF:
+            buildingSh.write(idx+1, 0, str(row[0]))
+            buildingSh.write(idx+1, 1, str(row[1]))
+            buildingSh.write(idx+1, 2, row[2])
+            buildingSh.write(idx+1, 3, row[3])
+            buildingSh.write(idx+1, 4, row[4])
+            buildingSh.write(idx+1, 5, row[5])
+            buildingSh.write(idx+1, 6, row[7])
+            buildingSh.write(idx+1, 7, row[8])
+            idx += 1
 
-#TODO preethivi: [optional] create a dashboard with visualization for the feedback data
+        workbook.save(output)
+        output.seek(0)
 
-#TODO jivi: connect server with db
+        return Response(output, mimetype="application/ms-excel", headers={"Content-Disposition": "attachment;filename=feedbacks.xls"})
 
-#TODO jivi: create route to get specific data from db
+# TODO preethivi: integrate the QR code function to the server and Generate QR code when they hit sumbit
+
+# TODO preethivi: add a route to display the feedback data
+
+# TODO preethivi: in the feedback display route, add a filter to select feedbacks between selected date
+
+# TODO preethivi: [optional] create a dashboard with visualization for the feedback data
+
+# TODO jivi: connect server with db
+
+# TODO jivi: create route to get specific data from db
+
 
 if __name__ == '__main__':
-    #DEBUG is SET to TRUE. CHANGE FOR PROD
-    app.run(port=5000,debug=True)
+    # DEBUG is SET to TRUE. CHANGE FOR PROD
+    app.run(port=5000, debug=True)
